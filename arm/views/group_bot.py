d7399369e35419e5c73436c6b72965a3a4e98e83
mrchi <i@mrchi.cc>
2024-03-29 02:08:02 +0800

    Use cached token

--- arm/views/group_bot.py
+++ arm/views/group_bot.py
@@ -1,65 +1,21 @@
 import json
-import os
-import threading
 import time
 import uuid
-from typing import TypedDict
 
 import httpx
 from fastapi import APIRouter, BackgroundTasks, Request
 
+from arm.libs.feishu_auth import FEISHU_AUTH
 from arm.libs.openai_rpc import evaluate_text_friendly_level, rewrite_to_friendly_text
 
 router = APIRouter()
-
-cache = threading.local()
-
-LEVEL_NAMES_MAP = {"A": "如沐春风", "B": "如水相安", "C": "剑拔弩张"}
-
-
-class AccessToken(TypedDict):
-    code: int  # 错误码，非 0 取值表示失败
-    msg: str  # 错误描述
-    tenant_access_token: str  # 租户访问凭证
-    expire: int  # tenant_access_token 的过期时间，单位为秒, like 7200
-
-
-class AccessTokenCache(TypedDict):
-    expire_timestamp: int
-    access_token: AccessToken
-
-
-def _get_access_token():
-    app_id = os.getenv("FEISHU_APP_ID")
-    app_secret = os.getenv("FEISHU_APP_SECRET")
-    url = "https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal"
-
-    response = httpx.post(
-        url,
-        headers={"Content-Type": "application/json"},
-        json={"app_id": app_id, "app_secret": app_secret},
-    )
-
-    response.raise_for_status()
-
-    return response.json()
-
-
-def get_acccess_token() -> str:
-    if not hasattr(cache, "access_token") or cache.access_token[
-        "expire_timestamp"
-    ] < int(time.time()):
-        cache.access_token = {
-            "expire_timestamp": int(time.time()) + 7200,
-            "access_token": _get_access_token(),
-        }
-    return cache.access_token["access_token"]["tenant_access_token"]
 
 
 def delete_msg(msg_id: str):
     url = f"https://open.feishu.cn/open-apis/im/v1/messages/{msg_id}"
     response = httpx.delete(
-        url, headers={"Authorization": f"Bearer {get_acccess_token()}"}
+        url,
+        headers={"Authorization": f"Bearer {FEISHU_AUTH.get_tenant_access_token()}"},
     )
     content = response.text
     print(content)
@@ -84,10 +40,9 @@ def make_new_group(data: dict):
     body = {
         "user_id_list": [data["event"]["sender"]["sender_id"]["open_id"]],
     }
-
     response = httpx.post(
         url,
-        headers={"Authorization": f"Bearer {get_acccess_token()}"},
+        headers={"Authorization": f"Bearer {FEISHU_AUTH.get_tenant_access_token()}"},
         params=params,
         json=body,
     )
@@ -102,7 +57,7 @@ def get_user_info(open_id: str):
     }
     response = httpx.get(
         url,
-        headers={"Authorization": f"Bearer {get_acccess_token()}"},
+        headers={"Authorization": f"Bearer {FEISHU_AUTH.get_tenant_access_token()}"},
         params=params,
     )
     body = response.json()
@@ -129,7 +84,7 @@ def send_msg(data: dict, text: str):
     response = httpx.post(
         url,
         headers={
-            "Authorization": f"Bearer {get_acccess_token()}",
+            "Authorization": f"Bearer {FEISHU_AUTH.get_tenant_access_token()}",
             "Content-Type": "application/json",
         },
         params=params,
@@ -140,7 +95,6 @@ def send_msg(data: dict, text: str):
 
 
 def evaluate_and_rewrite_msg(data: dict):
-    import time
 
     start = time.time()
 
@@ -156,14 +110,14 @@ def evaluate_and_rewrite_msg(data: dict):
     evaluate_end = time.time()
     print(f"evaluate cost: {evaluate_end - start}")
     if level in ["A", "B"]:
-        print(f"Level {level}/({LEVEL_NAMES_MAP[level]})")
+        print(f"Level {level})")
         return
     msg_id = data["event"]["message"]["message_id"]
     delete_msg(msg_id)
     output = rewrite_to_friendly_text(text)
     rewrite_end = time.time()
     print(f"rewrite cost: {rewrite_end - evaluate_end}")
-    res = {"level": LEVEL_NAMES_MAP[level], "output": output}
+    res = {"level": level, "output": output}
     print(res)
     send_msg(data, output)
     end = time.time()
@@ -178,7 +132,7 @@ def process_event(data: dict):
     evaluate_and_rewrite_msg(data)
 
 
-@router.post("")
+@router.post("/webhook")
 async def webhook(request: Request, background_tasks: BackgroundTasks):
     body = await request.body()
     print(body)
